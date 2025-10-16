26760fa1d0a5f61145e0d57da8ccc4d5d1204e0b
Luigi Rybarcik <ederson_lima2011@hotmail.com>
2025-10-15 21:49:27 -0300

    emails e testes

--- src/main/java/app/pet_pode_back/service/EmailService.java
+++ src/main/java/app/pet_pode_back/service/EmailService.java
@@ -1,23 +1,39 @@
 package app.pet_pode_back.service;
+//import com.sendgrid.*;
 
-import jakarta.mail.internet.MimeMessage;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+//import java.io.IOException;
+
 
 @Service
 public class EmailService {
 
-    @Autowired
-    private JavaMailSender mailSender;
+    @Value("${sendgrid.api.key}")
+    private String sendgridApiKey;
 
-    public void enviarEmail(String para, String assunto, String texto) {
-        SimpleMailMessage mensagem = new SimpleMailMessage();
-        mensagem.setTo(para);
-        mensagem.setSubject(assunto);
-        mensagem.setText(texto);
-        mailSender.send(mensagem);
-    }
+  //  public void enviarEmail(String para, String assunto, String texto) {
+//        Email from = new Email("petpodeoficial@gmail.com");
+//        Email to = new Email(para);
+//        Content content = new Content("text/plain", texto);
+//        Mail mail = new Mail(from, assunto, to, content);
+//
+//        SendGrid sg = new SendGrid(sendgridApiKey);
+//        Request request = new Request();
+//
+//        try {
+//            request.setMethod(Method.POST);
+//            request.setEndpoint("mail/send");
+//            request.setBody(mail.build());
+//            Response response = sg.api(request);
+//
+//            System.out.println("STATUS: " + response.getStatusCode());
+//            System.out.println("BODY: " + response.getBody());
+//            System.out.println("HEADERS: " + response.getHeaders());
+//        } catch (IOException ex) {
+//            System.err.println("Erro ao enviar e-mail com SendGrid:");
+//            ex.printStackTrace();
+//        }
+   // }
 }
